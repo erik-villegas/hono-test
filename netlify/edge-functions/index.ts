0ce66c50ee18653294ce7d610f8dff182b5e77f0
Erik Villegas <erik.villegas@catperson.com>
2023-09-09 13:41:36 -0700

    update

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -1,10 +1,12 @@
-import { handle } from 'https://deno.land/x/hono@v3.5.4/adapter/netlify/mod.ts'
-import { Hono } from 'https://deno.land/x/hono@v3.5.4/mod.ts'
+import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { handle } from 'https://deno.land/x/hono/adapter/netlify/mod.ts'
 
 const app = new Hono()
 
-app.get('/country', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/country', (c) =>
+  c.json({
+    message: 'Hello Netlify!',
+  })
+)
 
 export default handle(app)
